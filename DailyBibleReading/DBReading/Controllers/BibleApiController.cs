93c5aa972b42a605f01f069abc74f88c6c25ed5f
levj1 <levj1@yahoo.com>
2017-01-05 17:01:41 -0500

    Api call to display raw html
    Get html from json and display it to site.

--- DailyBibleReading/DBReading/Controllers/BibleApiController.cs
+++ DailyBibleReading/DBReading/Controllers/BibleApiController.cs
@@ -1,43 +1,42 @@
-﻿using DBReading.Models;
-using DBReading.Models.Book;
+﻿using DBReading.Models.Book;
+using DBReading.Models.Passage;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 
 namespace DBReading.Controllers
 {
     public class BibleApiController : Controller
     {
-        static string _address1 = @"https://bibles.org/v2/passages.js?q[]=john+3:1-5&version=eng-KJVA";
+        static string _passageURL = @"https://bibles.org/v2/fre-lsg/passages.js?q[]=john+3%3A1-5";
+        static string _BookURL = @"https://bibles.org/v2/versions/eng-GNTD/books.js";        
         static string _password = "mamajames1226";
         static string _token = "hCqGoQYlBorqkIyQnjpMSlqzx1Q1YEAUZaJMCrXN";
 
         // GET: BibleApi
         public async Task<ActionResult> Index()
         {
-            Rootobject_Book book = null;
+            Rootobject_Passage book = null;
             using (var client = new HttpClient())
             {
                 var credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", _token, _password)));
-                var uri = new Uri(_address1);
+                var uri = new Uri(_passageURL);
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
 
                 var response = await client.GetAsync(uri);
                 string textResult = await response.Content.ReadAsStringAsync();
                 if (response.IsSuccessStatusCode)
                 {
-                    book = JsonConvert.DeserializeObject<Rootobject_Book>(textResult);
+                    book = JsonConvert.DeserializeObject<Rootobject_Passage>(textResult);
+                    return View(book);      
                 }
+                return Content("An error occur: " + response); 
             }
-            return View(book);
-            
+                       
         }
 
         private async Task<Rootobject_Book> GetAllBooks()
@@ -46,7 +45,7 @@ namespace DBReading.Controllers
             using (var client = new HttpClient())
             {
                 var credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", _token, _password)));
-                var uri = new Uri(_address1);
+                var uri = new Uri(_BookURL);
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
 
                 var response = await client.GetAsync(uri);
