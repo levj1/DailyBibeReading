150f493d41234192f68a67fb0479b31f541981e1
levj1 <levj1@yahoo.com>
2017-01-16 16:14:11 -0500

    reading generator continued

--- DailyBibleReading/DBReading/Controllers/GeneratePlanController.cs
+++ DailyBibleReading/DBReading/Controllers/GeneratePlanController.cs
@@ -1,8 +1,11 @@
 ﻿using DBReading.Models;
+using DBReading.Models.Book;
+using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace DBReading.Controllers
@@ -19,10 +22,30 @@ namespace DBReading.Controllers
             _context.Dispose();
         }
         // GET: GeneratePlan
-        public ActionResult Index()
-        {
+        static string _passageURL = @"https://bibles.org/v2/fre-lsg/passages.js?q[]=john+3%3A1-5";
+        static string _BookURL = @"https://bibles.org/v2/versions/eng-GNTD/books.js";
+        static string _password = "mamajames1226";
+        static string _token = "hCqGoQYlBorqkIyQnjpMSlqzx1Q1YEAUZaJMCrXN";
 
-            return View();
+        // GET: BibleApi
+        public async Task<ActionResult> Index()
+        {
+            Rootobject_Book book = null;
+            using (var client = new HttpClient())
+            {
+                var credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", _token, _password)));
+                var uri = new Uri(_BookURL);
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
+
+                var response = await client.GetAsync(uri);
+                string textResult = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    book = JsonConvert.DeserializeObject<Rootobject_Book>(textResult);
+                    return View(book);
+                }
+                return Content("An error occur: " + response);
+            }
         }
     }
 }
